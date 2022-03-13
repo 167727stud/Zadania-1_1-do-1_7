cd8c68d8b1c743fe1ec9e013219a654179ed5d16
167727 <100698238+167727stud@users.noreply.github.com>
2022-03-13 16:03:02 +0000

    1_6 Liczby pierwsze  może nie pierwsze

--- Main.java
+++ Main.java
@@ -3,11 +3,33 @@ class Main {
   
   public static void main(String[] args) 
 { 
-  for(int lel=0;lel<101;lel++)
-    {
-      if ( lel % 2 == 0 ){
-        System.out.println("XD"+lel);
+  double pep;
+    System.out.print( "jaką liczbę chcesz sprawdzić czy jest pierwsza");
+   System.out.print(System.lineSeparator());
+  Scanner myInput = new Scanner( System.in );
+  pep = myInput.nextInt();
+  double lel = Math.sqrt(pep);
+  if(pep<11){
+    for(int kek=2;kek<pep;kek++)//przypadek dla małych liczb
+      {
+        if(pep % kek == 0)
+        {
+          System.out.println("No nie wiem , chyba ta"+
+                             " liczba nie jest pierwsza ");
+        return ;
         }
+      }
+    System.out.println("Brawo trafiłeś w liczbę pierwszą"); 
     }
+  else{//przypadek dla dużych liczb
+  for(int kek=2;kek<lel;kek++)
+    {
+      if ( pep % kek  == 0 )
+      {
+        System.out.println("No nie wiem , chyba ta liczba nie jest pierwsza ");
+        return ;
+      }
+      }
+  System.out.println("Brawo trafiłeś w liczbę pierwszą");
+    }}}
 
-}}
