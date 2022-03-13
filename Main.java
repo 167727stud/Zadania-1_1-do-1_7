a5eb9579064a5e2a7533178da2c1438df8ef2ac9
167727 <100698238+167727stud@users.noreply.github.com>
2022-03-13 15:08:24 +0000

    1_5 Parzyste do 100

--- Main.java
+++ Main.java
@@ -3,65 +3,11 @@ class Main {
   
   public static void main(String[] args) 
 { 
-  int pep;
-  double lel;
-  double kek;
-  double rer;
-  System.out.print( "Wciśnij odpowiednią liczbe");
-   System.out.print(System.lineSeparator());
-  System.out.print( "1 – dla sumy") ;
-   System.out.print(System.lineSeparator());
-  System.out.print( "2 – dla różnicy");
-   System.out.print(System.lineSeparator());
-  System.out.print( "3 – dla iloczynu");
-   System.out.print(System.lineSeparator());
-  System.out.print( "4 – dla ilorazu");
-   System.out.print(System.lineSeparator());
-    System.out.print( "inna – Wyjdź");
-   System.out.print(System.lineSeparator());
-  Scanner myInput = new Scanner( System.in );
-  pep = myInput.nextInt();
-  
-  switch(pep){
-    case 1:
-  
-  System.out.print( "wpisz pierwszą liczbe " );
-lel = myInput.nextInt();
-  System.out.print( "wpisz Drugą liczbe " );
-kek = myInput.nextInt();
-  rer = lel+kek;
-    System.out.println("suma to "+rer);
-      break;
-          case 2:
+  for(int lel=0;lel<101;lel++)
+    {
+      if ( lel % 2 == 0 ){
+        System.out.println("XD"+lel);
+        }
+    }
 
-  System.out.print( "wpisz pierwszą liczbe " );
-lel = myInput.nextInt();
-  System.out.print( "wpisz Drugą liczbe " );
-kek = myInput.nextInt();
- rer = lel-kek;
-    System.out.println("suma to "+rer);
-      break;
-          case 3:
- 
-  System.out.print( "wpisz pierwszą liczbe " );
-lel = myInput.nextInt();
-  System.out.print( "wpisz Drugą liczbe " );
-kek = myInput.nextInt();
-rer = lel*kek;
-    System.out.println("suma to "+rer);
-      break;
-          case 4:
-
-  System.out.print( "wpisz pierwszą liczbe " );
-lel = myInput.nextInt();
-  System.out.print( "wpisz Drugą liczbe " );
-kek = myInput.nextInt();
-rer = lel/kek;
-    System.out.println("suma to "+rer);
-      break;
-    default:
-      break;
-             }
-  }
-  //ble
-}
+}}
