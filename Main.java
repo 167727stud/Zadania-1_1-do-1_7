2be9a3c6743767381ddd6e1736e7205adac1191b
167727 <100698238+167727stud@users.noreply.github.com>
2022-03-13 14:25:01 +0000

    Zadanie 1_3

--- Main.java
+++ Main.java
@@ -1,8 +1,15 @@
+import java.util.Scanner;
 class Main {
+  
   public static void main(String[] args) 
 { 
-    int lel = 17 ;
-    System.out.println("Wypisz zmienną "+lel);
+  Scanner myInput = new Scanner( System.in );
+  System.out.print( "wpisz pierwszą liczbe " );
+int lel = myInput.nextInt();
+  System.out.print( "wpisz Drugą liczbe " );
+int kek = myInput.nextInt();
+  int rer = lel+kek;
+    System.out.println("suma to "+rer);
   }
   //ble
 }
